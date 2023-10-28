537e1a4c601ba1afabc832929aabcfcdee23595a
Jiaye Wei <105776528+jiaye-W@users.noreply.github.com>
2023-10-28 23:10:05 +0000

    Finish Ch2

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -5,8 +5,45 @@ public class Employee {
     public Employee() {}
 
     // YOUR CODE GOES HERE
+    private readonly int _id;
+
+    public required string? FullName{
+        get; set;
+    }
+
+    public required string? Department{
+        get; set;
+    }
+
+    public required int ID{
+        get => _id; 
+        init => _id = value;
+    }
+
+    public virtual void AdjustPay(decimal percentage){}
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
 // DEFINE OTHER CLASSES HERE
+class HourlyEmployee : Employee{
+    public decimal PayRate{
+        get; set;
+    }
+
+    public override void AdjustPay(decimal percentage)
+    {
+        PayRate += PayRate * percentage;
+    }
+}
+
+class SalariedEmployee : Employee{
+    public decimal Salary{
+        get; set;
+    }
+
+    public override void AdjustPay(decimal percentage)
+    {
+        Salary += Salary * percentage;
+    }
+}
